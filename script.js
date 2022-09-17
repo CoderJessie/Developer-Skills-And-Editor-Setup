5beb24284db5351dff9ad844ab2a11773a39164a
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-17 17:06:23 +0100

    introducing a bug and using debuger in chrome

--- script.js
+++ script.js
@@ -93,8 +93,8 @@ const calcTempAmplitudeBug = function (t1, t2) {
 	const temps = t1.concat(t2);
 	console.log(temps);
 
-	let max = temps[0];
-	let min = temps[0];
+	let max = 0;
+	let min = 0;
 
 	for (let i = 0; i < temps.length; i++) {
 		const curTemp = temps[i];
@@ -107,4 +107,5 @@ const calcTempAmplitudeBug = function (t1, t2) {
 	return max - min;
 };
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
 console.log(amplitudeBug);
