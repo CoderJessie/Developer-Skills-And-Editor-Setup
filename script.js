e71caf6fcb9c5605fbef2be3a9cef25a271a7978
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-17 15:58:00 +0100

    identifying, finding and fixing  bug

--- script.js
+++ script.js
@@ -70,17 +70,19 @@ const measureKelvin = function () {
 	const measurement = {
 		type: 'temp',
 		unit: 'celsius',
-		value: prompt('Degrees celsius:'),
+		// C) FIX
+		value: Number(prompt('Degrees celsius:')),
 	};
 
-	console.log(measurement);
+	// B) FIND
+	console.table(measurement);
 
-	console.log(measurement.value);
-	console.warn(measurement.value);
-	console.error(measurement.value);
+	// console.log(measurement.value);
+	// console.warn(measurement.value);
+	// console.error(measurement.value);
 
 	const kelvin = measurement.value + 273;
 	return kelvin;
 };
-
+// A) IDENTIFY
 console.log(measureKelvin());
