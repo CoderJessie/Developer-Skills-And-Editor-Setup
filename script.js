85e10d8f5445660afbf1c5f5999483a2cc766e4d
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-17 16:33:25 +0100

    Using a debugger

--- script.js
+++ script.js
@@ -71,7 +71,8 @@ const measureKelvin = function () {
 		type: 'temp',
 		unit: 'celsius',
 		// C) FIX
-		value: Number(prompt('Degrees celsius:')),
+		// value: Number(prompt('Degrees celsius:')),
+		value: 10,
 	};
 
 	// B) FIND
@@ -86,3 +87,24 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY
 console.log(measureKelvin());
+
+//Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+	const temps = t1.concat(t2);
+	console.log(temps);
+
+	let max = temps[0];
+	let min = temps[0];
+
+	for (let i = 0; i < temps.length; i++) {
+		const curTemp = temps[i];
+		if (typeof curTemp !== 'number') continue;
+
+		if (curTemp > max) max = curTemp;
+		if (curTemp < min) min = curTemp;
+	}
+	console.log(max, min);
+	return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
